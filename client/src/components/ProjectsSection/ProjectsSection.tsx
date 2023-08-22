badf95ce3125176b601486a7d4f7f379c6362fe5
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2023-08-22 11:24:19 -0400

    added some effects to projects section

--- client/src/components/ProjectsSection/ProjectsSection.tsx
+++ client/src/components/ProjectsSection/ProjectsSection.tsx
@@ -1,6 +1,25 @@
 import { useEffect, useState } from "react";
 import "./ProjectsSection.css";
 const ProjectsSection = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const [checkHovered, setCheckHovered] = useState(false);
+  let projectCards: any = document.querySelectorAll(".project-card");
+  let cardWidth = projectCards[0]?.offsetWidth || 0;
+  const handleCarouselScroll = (scrollDirection: string) => {
+    if (scrollDirection === "next") {
+      setScrollPosition((prevPosition: any) => {
+        return Math.min(
+          prevPosition + cardWidth,
+          cardWidth * (projectCards.length - 1)
+        );
+      });
+    } else if (scrollDirection === "prev") {
+      setScrollPosition((prevPosition: any) => {
+        return Math.max(prevPosition - cardWidth, 0);
+      });
+    }
+  };
+
   useEffect(() => {
     const projectCards: any = document.querySelectorAll(".project-card");
     let mid = Math.floor(projectCards.length / 2);
@@ -15,45 +34,87 @@ const ProjectsSection = () => {
     };
 
     projectCards.forEach((projectCard: any, index: number) => {
-      if (index === mid) {
-        projectCard.classList.add("project-card-active");
-      }
-      projectCard.addEventListener("mouseover", () => handleMouseOver(index));
+      // if (index === mid) {
+      //   projectCard.classList.add("project-card-active");
+      // }
+      projectCard.addEventListener("mouseover", () => {
+        handleMouseOver(index);
+      });
     });
 
     return () => {
       projectCards.forEach((projectCard: any, index: number) => {
-        projectCard.removeEventListener("mouseover", () =>
-          handleMouseOver(index)
-        );
+        projectCard.removeEventListener("mouseover", () => {
+          handleMouseOver(index);
+        });
       });
     };
   }, []);
+
   return (
     <section id="project-section">
       <h1>Projects</h1>
       <div className="project-section-container">
-        <div className="project-card">
+        <button
+          onClick={() => handleCarouselScroll("prev")}
+          id="prev-btn"
+          style={{
+            zIndex: `${checkHovered ? "0" : "10"}`,
+          }}
+        >
           <img
-            src="https://camo.githubusercontent.com/b3707f8d0a81f5f40fc3efa572cadf8c803e2a8b5e8ddbc5fbaff4e34c399e12/68747470733a2f2f692e6962622e636f2f437453767a76642f696d6167652e706e67"
-            alt="project1"
+            width="50"
+            height="50"
+            src="https://img.icons8.com/ios/50/000000/forward--v1.png"
+            alt="forward--v1"
           />
-          <h2>Rythmic</h2>
+        </button>
+        <div
+          className="project-section-inner"
+          style={{
+            display: "flex",
+            transform: `translateX(-${scrollPosition}px)`,
+            transition: "transform 0.5s",
+          }}
+        >
+          <div
+            className="project-card"
+            onMouseOver={() => {
+              setCheckHovered(true);
+            }}
+            onMouseLeave={() => {
+              setCheckHovered(false);
+            }}
+          >
+            <img
+              src="https://camo.githubusercontent.com/b3707f8d0a81f5f40fc3efa572cadf8c803e2a8b5e8ddbc5fbaff4e34c399e12/68747470733a2f2f692e6962622e636f2f437453767a76642f696d6167652e706e67"
+              alt="project1"
+            />
+            <h2>Rythmic</h2>
+          </div>
+          <div className="project-card">
+            <img
+              src="https://i.ytimg.com/vi/HggSXt1Hzfk/maxresdefault.jpg"
+              alt="project2"
+            />
+            <h2>Chat App</h2>
+          </div>
+          <div className="project-card">
+            <img
+              src="https://nativebase.io/img/notion/online-ticket-booking-universal-app.jpg"
+              alt="project3"
+            />
+            <h2>BookTheBest</h2>
+          </div>
         </div>
-        <div className="project-card">
+        <button onClick={() => handleCarouselScroll("next")} id="next-btn">
           <img
-            src="https://i.ytimg.com/vi/HggSXt1Hzfk/maxresdefault.jpg"
-            alt="project2"
+            width="50"
+            height="50"
+            src="https://img.icons8.com/ios/50/000000/forward--v1.png"
+            alt="forward--v1"
           />
-          <h2>Chat App</h2>
-        </div>
-        <div className="project-card">
-          <img
-            src="https://nativebase.io/img/notion/online-ticket-booking-universal-app.jpg"
-            alt="project3"
-          />
-          <h2>BookTheBest</h2>
-        </div>
+        </button>
       </div>
     </section>
   );
