34ff97794255eddd7f9ef70a50148dc3eb030b9f
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2023-12-20 16:13:48 +0530

    ch6

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,16 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  
+  build:{
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+          "react-dom": "ReactDOM",
+        },
+      },
+    },
+  },
+  plugins: [react()]
 })
