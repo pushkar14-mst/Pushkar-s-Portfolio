f9f6f5db6be9e2c136416b1731e9630fef9c8e08
Pushkar Patil <pushkar@Pushkars-MacBook-Pro.local>
2023-12-20 15:31:49 +0530

    ch4

--- client/src/components/ExperienceSection/ExperienceSection.tsx
+++ client/src/components/ExperienceSection/ExperienceSection.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useRef } from "react";
-import React from "react";
 import "./ExperienceSection.css";
 import { experiences } from "./experiences";
 const ExperienceSection = () => {
